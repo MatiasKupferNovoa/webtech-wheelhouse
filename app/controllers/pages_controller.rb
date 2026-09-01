dddf9ae4f602e606ca63c2cb069094d4137486fd
MatiasKupferNovoa <mjkupfer@miuandes.cl>
2026-09-01 18:46:59 -0400

    added pages, layout and bootstrap

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,11 @@ class PagesController < ApplicationController
             { name: "Brake adjustment", price: "$20000" },
             { name: "Brake pad replacement", price: "$25000" },
             { name: "Chain lubrication", price: "$15000" },
-            { name: "Chain replaecment", price: "$20000" },
+            { name: "Chain replacement", price: "$20000" },
+            { name: "Central axle lubrication", price: "$25000" },
+            { name: "Steering lubrication", price: "$22000" },
+            { name: "Fork maintenance", price: "$20000" },
+            { name: "Bicycle assembly", price: "$40000" },
             { name: "Basic maintenance", price: "$30000" },
             { name: "Complete maintenance", price: "$50000" }
         ]
